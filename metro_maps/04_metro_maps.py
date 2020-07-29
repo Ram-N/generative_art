5e689ef3f46cefe9d146e38bbefc9b05a493aa8d
Ram-N <ramnarasimhan@gmail.com>
2020-07-29 17:01:18 -0400

    MetroMapper Added

--- metro_maps/04_metro_maps.py
+++ metro_maps/04_metro_maps.py
@@ -1,30 +1,37 @@
 """
-
 Metro Maps for imaginary cities
-
-4th Script
-
-1. Overlaps
-2. Moved render_mm to a separate function
 
 Author: Ram Narasimhan
 July 2020
 
+4th Script
+
+- Overlaps have to be shown clearly
+1. Made segs to be lexicographically sorted
+2. Make the segments to be a dictionary of dicts. Track Lines, dir and incoming and outgoing.
+3. Have to connect the lines such that it flows without a break.
+4. Plot one line at a time. In each junction, its prev and next x and y values should be the same
+5. All overlaps have been addressed, but the x values for a particular line are not always the same. So it 'flips'
+
+
+
 """
 
 
-side = 50
+side = 45
 w = 2 * side
 ht = sqrt(3) * side
-margin = 60
+margin = 70
 sdim = 30
 stn_size = 10
 num_rows, num_cols = 9, 10
 
 RED, BLUE = (255, 0, 0), (0, 255, 0)
 GREEN, BLACK = (0, 0, 255), (0, 0, 0)
-CYAN, YELLOW = (255, 0, 255), (0, 255, 255)
-colors = [RED, BLUE, GREEN, BLACK, CYAN, YELLOW]
+PURPLE, CYAN, YELLOW = (255, 0, 255), (0, 255, 255), (255, 255, 0)
+ORANGE = (255, 150, 0)
+ROSE, TEAL = (255, 102, 204), (0, 128, 128)
+colors = [CYAN, ORANGE, BLUE, TEAL, GREEN, BLACK, PURPLE, RED, YELLOW, ROSE]
 next_available = 0
 
 
@@ -92,76 +99,39 @@ def get_next_node(curr_node, sd):
 
     if dir == (1, 1):
         rnd = random(1)
-        if rnd < 0.33:
+        if rnd < 0.2:
             return (curr_node[0] + 1, curr_node[1])
-        elif rnd < 0.67:
+        elif rnd < 0.4:
             return (curr_node[0], curr_node[1] + 1)
         else:
             return (curr_node[0] + 1, curr_node[1] + 1)
 
     if dir == (1, -1):
         rnd = random(1)
-        if rnd < 0.33:
+        if rnd < 0.2:
             return (curr_node[0] + 1, curr_node[1])
-        elif rnd < 0.67:
+        elif rnd < 0.4:
             return (curr_node[0], curr_node[1] - 1)
         else:
             return (curr_node[0] + 1, curr_node[1] - 1)
 
     if dir == (-1, -1):
         rnd = random(1)
-        if rnd < 0.33:
+        if rnd < 0.2:
             return (curr_node[0] - 1, curr_node[1])
-        elif rnd < 0.67:
+        elif rnd < 0.4:
             return (curr_node[0], curr_node[1] - 1)
         else:
             return (curr_node[0] - 1, curr_node[1] - 1)
 
     if dir == (-1, 1):
         rnd = random(1)
-        if rnd < 0.33:
+        if rnd < 0.2:
             return (curr_node[0] - 1, curr_node[1])
-        elif rnd < 0.67:
+        elif rnd < 0.4:
             return (curr_node[0], curr_node[1] + 1)
         else:
             return (curr_node[0] - 1, curr_node[1] + 1)
-
-
-def render_metro_map():
-
-    strokeWeight(8)
-    for l in lines.keys():
-        num_stn = len(lines[l])
-
-        mark_terminal_node(l, lines[l][0], 0)
-        mark_terminal_node(l, lines[l][-1], 1)
-
-        print("line", l, num_stn, lines[l])
-        for snum in range(num_stn - 1):
-            cx, cy = lines[l][snum]
-            nx, ny = lines[l][snum + 1]
-            stroke(*colors[l])
-
-            xadj, yadj = 0, 0
-
-            # check if coords need adjusting due to segments carrying multiple lines
-            if len(segments[((cx, cy), (nx, ny))]) > 1:
-                xadj, yadj = 10, 10
-
-            line(
-                xcoord(cx) + xadj,
-                ycoord(cy) + yadj,
-                xcoord(nx) + xadj,
-                ycoord(ny) + yadj,
-            )
-
-            if num_tracks[(cx, cy)] == 1:
-                fill(*colors[l])
-                ellipse(xcoord(cx), ycoord(cy), stn_size, stn_size)
-            elif num_tracks[(cx, cy)] > 1:
-                fill(255, 255, 255)
-                noStroke()
-                ellipse(xcoord(cx), ycoord(cy), stn_size * 3, stn_size * 2)
 
 
 def xcoord(xval):
@@ -183,23 +153,169 @@ def pick_orig_dest(num_rows, num_cols):
 
     done = False
     while not done:
+        s0 = (int(random(num_cols)), int(random(num_rows)))
         s1 = (int(random(num_cols)), int(random(num_rows)))
-        s2 = (int(random(num_cols)), int(random(num_rows)))
-        xd, yd = get_dist(s1, s2)
+        xd, yd = get_dist(s0, s1)
 
-        if station_d[s1] == "outer" or station_d[s2] == "outer":
+        if station_d[s0] == "outer" or station_d[s1] == "outer":
             done = True
 
         if abs(xd) + abs(yd) <= 5:
             done = False
 
-    return (s1, s2)
+        # lexisort such that origin is "before" destination
+        orig, dest = get_sorted_seg(s0[0], s0[1], s1[0], s1[1])
+
+    return (orig, dest)
+
+
+def get_sorted_seg(cx, cy, nx, ny):
+    """ Segments are sorted in numerically. """
+    if cx < nx:
+        return ((cx, cy), (nx, ny))
+    elif cx > nx:
+        return ((nx, ny), (cx, cy))
+    else:  # equal
+        if cy <= ny:
+            return ((cx, cy), (nx, ny))
+        else:
+            return ((nx, ny), (cx, cy))
+
+
+def get_seg_orientation(lseg):
+    """ is it - | / or \ """
+
+    cs, ns = lseg
+    cx, cy = cs
+    nx, ny = ns
+
+    if cx == nx:
+        return 90
+    if cy == ny:
+        return 0
+    if ny > cy:
+        return -45
+    if ny < cy:
+        return 45
+
+
+def render_metro_line(l):
+    """ Draw one route at a time. Mark stations and junctions as appropriate"""
+    strokeWeight(8)
+    num_stn = len(lines[l])
+
+    mark_terminal_node(l, lines[l][0], 0)
+    mark_terminal_node(l, lines[l][-1], 1)
+
+    # print('line', l, num_stn, lines[l])
+    for snum in range(num_stn - 1):
+        cx, cy = lines[l][snum]
+        nx, ny = lines[l][snum + 1]
+        stroke(*colors[l])
+
+        xadj, yadj = 0, 0
+
+        # check if coords need adjusting due to segments carrying multiple lines
+        sa, sb = get_sorted_seg(cx, cy, nx, ny)
+
+        if segments[(sa, sb)]["numtracks"] == 2:
+            pos = segments[(sa, sb)]["lines"].index(l)  # index among all the routes
+
+            if segments[(sa, sb)]["dir"] == 90:  # vertical segment
+                if pos == 0:
+                    xadj, yadj = -4, 0
+                if pos == 1:
+                    xadj, yadj = 4, 0
+            if segments[(sa, sb)]["dir"] == 0:  # horizontal segment
+                if pos == 0:
+                    xadj, yadj = 0, -4
+                if pos == 1:
+                    xadj, yadj = 0, 4
+            if segments[(sa, sb)]["dir"] == -45:  # Backslash segment
+                if pos == 0:
+                    xadj, yadj = -6, 0
+                if pos == 1:
+                    xadj, yadj = 4, 0
+
+            if segments[(sa, sb)]["dir"] == 45:  # Forward segment
+                if pos == 0:
+                    xadj, yadj = -4, -2
+                if pos == 1:
+                    xadj, yadj = 4, 2
+
+        # THREE TRACKS going through a station.
+        if segments[(sa, sb)]["numtracks"] == 3:
+            pos = segments[(sa, sb)]["lines"].index(l)  # index among all the routes
+
+            if segments[(sa, sb)]["dir"] == 90:  # vertical segment
+                if pos == 0:
+                    xadj, yadj = -8, 0
+                if pos == 1:
+                    xadj, yadj = 0, 0
+                if pos == 2:
+                    xadj, yadj = 8, 0
+
+            if segments[(sa, sb)]["dir"] == 0:  # horizontal segment
+                if pos == 0:
+                    xadj, yadj = 0, -8
+                if pos == 1:
+                    xadj, yadj = 0, 0
+                if pos == 2:
+                    xadj, yadj = 0, 8
+            if segments[(sa, sb)]["dir"] == -45:  # Backslash segment
+                if pos == 0:
+                    xadj, yadj = -10, 2
+                if pos == 1:
+                    xadj, yadj = 0, 0
+                if pos == 2:
+                    xadj, yadj = +10, 2
+
+            if segments[(sa, sb)]["dir"] == 45:  # Forward segment
+                if pos == 0:
+                    xadj, yadj = -8, -2
+                if pos == 1:
+                    xadj, yadj = 0, 0
+                if pos == 2:
+                    xadj, yadj = +8, 2
+
+        line(xcoord(cx) + xadj, ycoord(cy) + yadj, xcoord(nx) + xadj, ycoord(ny) + yadj)
+
+        # mark the stations for this line
+        if num_tracks[(cx, cy)] == 1:
+            fill(*colors[l])
+            ellipse(xcoord(cx), ycoord(cy), stn_size, stn_size)
+        # render the Junctions for this line
+        elif num_tracks[(cx, cy)] > 1:
+            fill(255, 255, 255)
+            noStroke()
+            ellipse(xcoord(cx), ycoord(cy), stn_size * 3, stn_size * 2)
+
+
+def get_clock_dir(prev_node, curr_node):
+
+    if prev_node == (-1, -1):
+        return -1
+    xdiff, ydiff = get_dist(prev_node, curr_node)
+    # print(xdiff, ydiff, 'prev', prev_node, 'curr', curr_node)
+    if xdiff > 0 and ydiff > 0:
+        return 4  # sloped but downward pointing
+    if xdiff > 0 and ydiff < 0:
+        return 1  # slope upward pointing
+    if xdiff > 0 and ydiff == 0:
+        return 3
+    if xdiff == 0 and ydiff > 0:
+        return 6
+    if xdiff == 0 and ydiff < 0:
+        return 0
+
+    return -1  # something is not right.
 
 
 def create_route(route_num, so, sd):
     global lines, num_tracks, segments
 
     curr_node = so
+    prev_node = (-1, -1)
     cx, cy = so
     lines[route_num] = [(cx, cy)]
     num_tracks[(cx, cy)] += 1
@@ -207,19 +323,61 @@ def create_route(route_num, so, sd):
     while curr_node != sd:
         nx, ny = get_next_node(curr_node, sd)
         lines[route_num].append((nx, ny))
-        if ((cx, cy), (nx, ny)) in segments:
-            segments[((cx, cy), (nx, ny))].append(route_num)
+
+        # Adding routes to Segments. This helps catch overlaps
+        lseg = get_sorted_seg(cx, cy, nx, ny)
+        if (lseg) in segments:
+            segments[lseg]["lines"].append(route_num)
+            segments[lseg]["numtracks"] += 1
+            segments[lseg]["start"].append(
+                (route_num, get_clock_dir(prev_node, curr_node))
+            )
+
         else:
-            segments[((cx, cy), (nx, ny))] = [route_num]
+            segments[lseg] = {}  # new segment found. Let's record its characteristics
+            segments[lseg]["numtracks"] = 1
+            segments[lseg]["lines"] = [route_num]
+            segments[lseg]["dir"] = get_seg_orientation(lseg)
+            segments[lseg]["start"] = [(route_num, get_clock_dir(prev_node, curr_node))]
 
         num_tracks[(nx, ny)] += 1
         # stroke(*colors[next_available])
         # strokeWeight(8)
         # line(xcoord(cx), ycoord(cy), xcoord(nx), ycoord(ny))
+        prev_node = curr_node  # store the previous
         curr_node = (nx, ny)
         cx, cy = curr_node
 
-    # draw_line_from(so, sd)
+    line_length = len(lines[route_num])
+    # prev_stn = (-1, -1)
+    for stn in range(line_length - 2):
+        curr_seg_start = lines[route_num][stn]
+        curr_seg_end = lines[route_num][stn + 1]
+        next_stn = lines[route_num][stn + 2]
+
+        curr_seg = get_sorted_seg(
+            curr_seg_start[0], curr_seg_start[1], curr_seg_end[0], curr_seg_end[1]
+        )
+        next_seg = get_sorted_seg(
+            curr_seg_end[0], curr_seg_end[1], next_stn[0], next_stn[1]
+        )
+        print(curr_seg, "next seg", next_seg)
+        if "end" in segments[curr_seg]:
+            segments[curr_seg]["end"].append(
+                (route_num, get_clock_dir(next_seg[0], next_seg[1]))
+            )
+        else:
+            # print('seg end', route_num, curr_stn, 'next_stn', next_stn, get_clock_dir(curr_stn, next_stn))
+            segments[curr_seg]["end"] = [
+                (route_num, get_clock_dir(next_seg[0], next_seg[1]))
+            ]
+
+    # The TERMINAL segment should get an "end" of -1
+    if "end" in segments[next_seg]:
+        segments[next_seg]["end"].append((route_num, -1))
+    else:
+        # print('seg end', route_num, curr_stn, 'next_stn', next_stn, get_clock_dir(curr_stn, next_stn))
+        segments[next_seg]["end"] = [(route_num, -1)]
 
 
 station_d = {}  # key is station (xy). value
@@ -228,28 +386,33 @@ num_tracks = (
     {}
 )  # key is station. Keeps track of how many routes go through each station
 segments = {}  # k: segments, v: list of Lines
-num_lines = 6
+num_lines = 9
+border = 25
 
 
 def setup():
     global next_available, station_d, lines, num_tracks, segments
 
     size(1000, 900)
-    background(128)
+    background(250)
+    fill(170)
+    rect(border, border, width - 2 * border, height - 2 * border)
     create_hex_grid(margin)
 
     for route_num in range(num_lines):
 
         so, sd = pick_orig_dest(num_rows, num_cols)
-        print(so, sd, "dest")
+
         create_route(route_num, so, sd)
 
-    # Draw it out
-    render_metro_map()
+    for route_num in range(num_lines):
+        # Draw the line, stations, and junctions
+        render_metro_line(route_num)
 
-    for k, v in segments.items():
-        if len(v) > 1:
-            print(k, v)
+    print("Overlapping Segs")
+    for k, d in segments.items():
+        if len(d["lines"]) > 1:
+            print(k, d, d["dir"])
 
 
 def draw():
--- metro_maps/README.md
+++ metro_maps/README.md
@@ -1,30 +1,40 @@
 # Metro Maps
 
+![Sample Map](images/mm_5.jpg)
+
 This project creates Metro Maps for imaginary cities.
 
+What would the Metro (subway) map look like, for imaginary cities? Each time the code is run, it produces a
+different set of Terminals, Routes, Junctions and Stations.
 
 **Xanadu Metro Map**
 
 1. Stations can only be placed on an invisible grid. Hexagonal centers?
-2. A "Line" goes from an O station to a D station. (O and D should be closer to the boundaries of the grid.
+2. A "Line" goes from an O station to a D station. (O and D should be closer to the boundaries of the grid.)
 3. Parallael lines run fat and adjacent to each other
-4. Some stations are "junctions" where the lines will cross each other
-5. Junctions are shown with a bigger circle with a colored border.
-6. All lines can only run at 90 or 45 degree lines.
+4. Some stations are "junctions" where the lines will cross each other. Show those with a white oval. These are
+places wherein passengers can switch lines.
+6. All lines can only run at 90 or 45 degree lines, a convention followed ever since the London Underground created its famous maps.
 
 **More Intricate Rules**
 1. Overlapping routes should be nicely aligned
 2. Minimal crossovers of routes
+3. Control the amount of 45 degree and 90 degree lines.
 
-
-**Dynamic**
-
+**Dynamic** (Not implemented)
 D1. Lines and Stations "disappear" over time, starting from origin until the destination disappears.
 D2. For every disappearance, a new station/node/line reemerges elsewhere.
 
-## Static Metro Maps
+Note: The rules for intersecting lines, overlapping segments and showing them elegantly got overly complex.
+So I haven't implemented the dynamic component yet. Might come back to it.
 
-These were the early gif-loop attempts
+## Static Metro Maps. Samples
 
-<img src="images/mm_wip1.png" width="250">
+<img src="images/mm_1.jpg" width="200">
+<img src="images/mm_2.jpg" width="200">
+<img src="images/mm_3.jpg" width="200">
+<img src="images/mm_4.jpg" width="200">
+<img src="images/mm_5.jpg" width="200">
+
+A few more examples can be found in the [images]('images') directory.
 
