128d33677a5f48294e0b99f41bef327be51ecb0e
Ram-N <ramnarasimhan@gmail.com>
2021-08-26 22:30:17 -0400

    2021-08-24/daily_sketch DW4

---
+++ daily_sketches/2021/2021-08-24/README.md
@@ -0,0 +1,35 @@
+# DAILY SKETCH for 2021-08-24
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-08-26-22-17-10.png' width = '100'> <img src = 'images/keep_2021-08-26-22-18-57.png' width = '100'> <img src = 'images/keep_2021-08-26-22-23-45.png' width = '100'> <img src = 'images/keep_2021-08-26-22-27-44.png' width = '100'> <img src = 'images/keep_2021-08-26-22-29-15.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-08-26-22-17-10.png' width = '300'> 
+<img src = 'images/keep_2021-08-26-22-18-57.png' width = '300'> 
+<img src = 'images/keep_2021-08-26-22-23-45.png' width = '300'> 
+<img src = 'images/keep_2021-08-26-22-27-44.png' width = '300'> 
+<img src = 'images/keep_2021-08-26-22-29-15.png' width = '300'> 
+
+
+[More Images](2021-08-24/images) 
+
+
+ ## 2021-08-24
+Keywords: Domain Warping, Perlin Noise, Layers
+ 
+
+## Description 
+
+ Panels of Domain Warping (cutoff-based) has a continent/map-like effect.
+ 
+
+Made using P5.js. | [Code](2021/2021-08-24/) | [Top](#daily-sketches) 
+
+-----
+
---
+++ daily_sketches/2021/2021-08-24/sketch.js
@@ -0,0 +1,115 @@
+// Daily Sketch for 2021-08-24
+// Ram Narasimhan
+
+/*
+Domain Warping
+Layers
+Repeating colors...
+*/
+
+
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+let palette = []
+const params = {
+    //    bgColor: "#F1E0EA",
+    //bgColor:"#EAB6AB"
+    bgColor: "#0f0f0f", //black
+    //    bgColor: '#ab3977', //black
+}
+
+function setup() {
+    createCanvas(660, 660);
+    colorMode(HSB);
+    background(0, 0, 0);
+
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+    print(cnv)
+    xstart = cnv.xMargin
+    ystart = cnv.yMargin
+    xend = cnv.xMargin + cnv.width
+    yend = cnv.yMargin + cnv.height
+    midX = width / 2
+    midY = height / 2
+
+    palette2 = red_brown_orange; //colors.js
+    palette = flame;
+    //palette = replicate(palette, 100)
+
+    //Custom Tiling
+    colSplit = [1, 1]
+    rowSplit = [3, 3]
+    pgrid = new PanelGrid(cnv, colSplit, rowSplit)
+    //pgrid.renderPanelGrid(10);
+
+    for (p of pgrid.panels) {
+        domainWarpInsidepanel(p)
+    }
+
+    draw_border(clr = params.bgColor, sw = 50); //rn_utils.js
+}
+
+numLayers = 3
+cutoff = [0.5, 0.5, 0.4]
+
+function domainWarpInsidepanel(panel, dw_properties) {
+
+    push();
+    translate(panel.x, panel.y);
+    for (layer = 0; layer < numLayers; layer++) {
+        noisebase = layer * 0.01 * int(random(1, 7))
+        distortion_strength = layer * 0.001 * int(random(1, 7));
+        for (x = 0; x <= panel.pw; x++) {
+            for (y = 0; y <= panel.ph; y++) {
+                noise_scale = noisebase + (panel.x) * 0.0001
+                distortion_strength += 1e-4
+                domain_warp(x, y, distortion_strength, noise_scale, layer)
+            }
+        }
+    }
+    pop();
+
+}
+
+function distort(x, y) {
+    wiggle = 0.02
+    return noise(x * wiggle, y * wiggle)
+}
+
+
+
+
+function domain_warp(x, y, distortion_strength, noise_scale, layer) {
+
+
+    dw_value = noise((x + 100 * layer) * 0.01, (y + layer * 233) * noise_scale)
+    if (color_mapping(dw_value, layer)) {
+        point(x, y)
+    }
+
+}
+
+startColrs = [180, 344, 22]
+
+function color_mapping(dw_value, layer) {
+    offset = layer * 20
+    startColr = startColrs[layer]
+    endColr = startColr + 80
+    colr = map(dw_value, 0, 1, startColr + offset, endColr + offset) % 360
+    if (dw_value < cutoff[layer]) {
+        stroke(colr, 50 + 10 * layer, 60 + 10 * layer)
+        return 1
+    }
+    return 0 // don't draw the point
+}
+
+
+
+
+
+
+
