66c5a3aa6f6ff896d730ab8efcf83df02bc21f62
Ram-N <ramnarasimhan@gmail.com>
2021-09-13 18:18:55 -0400

    2021-09-12/daily_sketch sprioGrid p variation

---
+++ daily_sketches/2021/2021-09-12/README.md
@@ -0,0 +1,36 @@
+# DAILY SKETCH for 2021-09-12
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-09-13-18-01-31.png' width = '100'> <img src = 'images/keep_2021-09-13-18-04-42.png' width = '100'> <img src = 'images/keep_2021-09-13-18-09-12.png' width = '100'> <img src = 'images/keep_2021-09-13-18-18-14.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-09-13-18-01-31.png' width = '300'> 
+<img src = 'images/keep_2021-09-13-18-04-42.png' width = '300'> 
+<img src = 'images/keep_2021-09-13-18-09-12.png' width = '300'> 
+<img src = 'images/keep_2021-09-13-18-18-14.png' width = '300'> 
+
+
+[More Images](2021-09-12/images) 
+
+
+ ## 2021-09-12
+Keywords: Spirograph, panel
+ 
+
+## Description 
+
+ Playing around with two radii (r1 and r2) and angle increments, on a 2x2 panel
+ Color variations and radii variations
+ http://www.eddaardvark.co.uk/python_patterns/spirograph.htm
+ 
+
+Made using P5.js. | [Code](2021/2021-09-12/) | [Top](#daily-sketches) 
+
+-----
+
---
+++ daily_sketches/2021/2021-09-12/sketch.js
@@ -0,0 +1,108 @@
+// Daily Sketch for 2021-09-12
+// Ram Narasimhan.
+/*
+
+Keywords: Spirograph
+http://www.eddaardvark.co.uk/python_patterns/spirograph.htm
+
+*/
+
+
+let palette = []
+let grid;
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+const params = {
+    xStep: 25,
+    yStep: 25,
+    r1: 80,
+    r2: 60,
+}
+
+
+function setup() {
+    createCanvas(960, 960);
+    background("#d3d3d3");
+    //background("#0F0F0F");
+    fill("#0f0f0f");
+    draw_border(20); //rn_utils.js
+    palette = random(palList);
+    palette = red_brown_orange;
+    //palette = cappuccino; //colors.js
+    //palette = rainbowDash; //colors.js
+    palette = replicate(palette, 100)
+    palette2 = take5; //colors.js
+    palette2 = replicate(palette2, 100)
+
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+
+    r1 = params.r1
+    r2 = params.r2
+
+    //Custom Tiling
+    colSplit = [1, 1, 1, 1, 1]
+    rowSplit = [1, 1, 1, 1, 1]
+    pgrid = new PanelGrid(cnv, colSplit, rowSplit)
+
+    angleMode(DEGREES);
+    strokeWeight(3)
+    stroke(palette[2]);
+    pcount = 0;
+    for (panel of pgrid.panels) {
+
+        let from = color(random(palette2));
+        let to = color(random(palette2));
+        let interB = lerpColor(from, to, 0.66);
+        fill(interB)
+        rect(panel.x, panel.y, panel.pw, panel.ph)
+        for (ph = 0; ph < 0.1; ph += 0.1) {
+            pcount += 2;
+            sp1(ph, pcount);
+        }
+    }
+    draw_border(clr = 20, sw = 20); //rn_utils.js
+
+
+}
+
+function sp1(phase, pcount) {
+    push();
+    translate(panel.cx, panel.cy);
+    r2 = int(random(1, 10));
+    r1 = r2 * int(random(12, 22));
+    r2 = 4;
+    r1 = 48;
+    p = int(random(5, 15));
+    p = pcount + 1;
+    print(r1, r2, p)
+    renderSpiro(r1, r2, p, phase)
+    pop();
+}
+
+
+
+//    x(t,R,r,p) = (R-r)*cos(t) + p*cos((R-r)*t/r)
+//   y(t,R,r,p) = (R-r)*sin(t) - p*sin((R-r)*t/r)
+
+function renderSpiro(r1, r2, p, phase = 0) {
+    let px = 0;
+    let py = 0;
+
+    aStep = 1;
+    a = 0;
+    px = (r1 - r2) * cos(a) + p * cos((r1 - r2) * a / r2)
+    py = (r1 - r2) * sin(a) + p * sin((r1 - r2) * a / r2)
+    for (a = phase; a <= 360 + phase; a += aStep) {
+        x = (r1 - r2) * cos(a) + p * cos((r1 - r2) * a / r2)
+        y = (r1 - r2) * sin(a) + p * sin((r1 - r2) * a / r2)
+        line(px, py, x, y)
+        px = x;
+        py = y;
+    }
+
+}
+
