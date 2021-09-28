108c2e04d562705a7b11073adaf2ebd6d9e6a226
Ram-N <ramnarasimhan@gmail.com>
2021-09-28 16:33:14 -0400

    2021-09-28/daily_sketch zonal colors

---
+++ daily_sketches/2021/2021-09-28/README.md
@@ -0,0 +1,35 @@
+# DAILY SKETCH for 2021-09-28
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-09-28-15-44-15.png' width = '100'> <img src = 'images/keep_2021-09-28-16-08-34.png' width = '100'> <img src = 'images/keep_2021-09-28-16-31-40.png' width = '100'> <img src = 'images/keep_2021-09-28-16-32-13.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-09-28-15-44-15.png' width = '300'> 
+<img src = 'images/keep_2021-09-28-16-08-34.png' width = '300'> 
+<img src = 'images/keep_2021-09-28-16-31-40.png' width = '300'> 
+<img src = 'images/keep_2021-09-28-16-32-13.png' width = '300'> 
+
+
+[More Images](2021-09-28/images) 
+
+
+ ## 2021-09-28
+Keywords: Fractal, subdivision, shapes
+ 
+
+## Description 
+
+ Fractal subdivision with random splits on 4 sides. Calculate midpoint and then connect.
+ Zonal Colors...
+ 
+
+Made using P5.js. | [Code](2021/2021-09-28/) | [Top](#daily-sketches) 
+
+-----
+
---
+++ daily_sketches/2021/2021-09-28/sketch.js
@@ -0,0 +1,211 @@
+// Daily Sketch for 2021-09-26
+// Ram Narasimhan.
+
+/*
+Keywords: fractals
+Addition: Add a stopping criteria for shapes that are too small...
+*/
+
+
+let palette = []
+let grid;
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+const params = {
+    xStep: 25,
+    yStep: 25,
+    bgColor: "#d3d3d3",
+    blkColor: "#0f0f0f",
+}
+
+class Shingle { // A roughly rectangular shape, made up of 4 points...
+    constructor(nw, ne, se, sw, pal) {
+        this.nw = nw; // a point with x and y
+        this.ne = ne;
+        this.se = se;
+        this.sw = sw;
+        this.render = 1;
+        this.vsize = dist(nw.x, nw.y, sw.x, sw.y)
+        this.hsize = dist(nw.x, nw.y, ne.x, ne.y)
+        this.open = 1; // still open to get split
+        this.palette = pal;
+    }
+
+    display(colr, swt = 1) {
+        strokeWeight(swt)
+        // if (colr) {
+        //     fill(colr);
+        // }
+        stroke(params.blkColor)
+        fill(random(this.palette))
+
+        beginShape()
+        vertex(this.nw.x, this.nw.y);
+        vertex(this.ne.x, this.ne.y);
+        vertex(this.se.x, this.se.y);
+        vertex(this.sw.x, this.sw.y);
+        endShape(CLOSE);
+
+    }
+
+
+}
+
+
+
+
+function setup() {
+    createCanvas(960, 960);
+    background(params.bgColor);
+    palette = cappuccino; //colors.js
+    palette = rainbowDash; //colors.js
+    palette = replicate(palette, 100)
+    palette2 = random(palList)
+    palette2 = replicate(palette2, 100)
+
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+    //fill("#0f0f0f");
+    //rect(cnv.xMargin, cnv.yMargin, cnv.width, cnv.height);
+
+    grid = new Grid(params.xStep, params.yStep, cnv.width, cnv.height, cnv.xMargin, cnv.yMargin);
+
+
+
+    //initialize one large shingle with 4 corners
+    let shingleList = [];
+    let nw = new Point(cnv.xMargin, cnv.yMargin)
+    let ne = new Point(cnv.xMargin + cnv.width, cnv.yMargin)
+    let se = new Point(cnv.xMargin + cnv.width, cnv.yMargin + cnv.height)
+    let sw = new Point(cnv.xMargin, cnv.yMargin + cnv.height)
+    let sh = new Shingle(nw, ne, se, sw, random(palList));
+    shingleList.push(sh);
+
+    numSubdivisions = 250
+    vSizeCutoff = 10
+    hSizeCutoff = 10
+
+    sh0 = random(shingleList);
+    subdivide(sh0, 0, shingleList);
+
+    for (s = 0; s <= numSubdivisions; s += 1) {
+        //pick a shingle to divide
+        sh0 = random(shingleList);
+        subdivide(sh0, 1, shingleList);
+    }
+
+
+
+    for (sh of shingleList) {
+        if (sh.render) {
+            sh.display(random(palette2), 5);
+            //print(sh.vsize, sh.hsize)
+        }
+    }
+
+    draw_border(clr = params.blkColor, sw = 29 + cnv.xMargin); //rn_utils.js
+    //    draw_border(clr = 20, sw = 20); //rn_utils.js
+
+    //midPt = getinBetweenPtOnLine(1, 1, 3, 3, 0.33)
+}
+
+
+/*
+To "process" a shingle is to 
+a. subdivide it into 4
+b. add the 4 new into the list
+c. mark the original shingle as done. It doesn't have to be displayed anymore. It's "children" will take over
+*/
+function subdivide(sh0, maintainColor, shingleList) {
+
+    if ((sh0.open) && (sh0.vsize > vSizeCutoff) && (sh0.hsize > hSizeCutoff)) { // untouched, so go ahead and subdivide
+        fracW = random(0.3, 0.7)
+        let mWest = getInbetweenPtOnLine(sh0.nw.x, sh0.nw.y, sh0.sw.x, sh0.sw.y, fracW)
+        fracE = random(0.3, 0.7)
+        let mEast = getInbetweenPtOnLine(sh0.ne.x, sh0.ne.y, sh0.se.x, sh0.se.y, fracE)
+        fracN = random(0.3, 0.7)
+        let mNorth = getInbetweenPtOnLine(sh0.nw.x, sh0.nw.y, sh0.ne.x, sh0.ne.y, fracN)
+        fracS = random(0.3, 0.7)
+        let mSouth = getInbetweenPtOnLine(sh0.sw.x, sh0.sw.y, sh0.se.x, sh0.se.y, fracS)
+
+
+        result = intersectionPtOf2Lines(mWest.x, mWest.y, mEast.x, mEast.y,
+            mNorth.x, mNorth.y, mSouth.x, mSouth.y)
+
+
+
+        let midX = result.x
+        let midY = result.y
+
+        if (maintainColor) {
+            pal = sh0.palette
+        } else {
+            pal = purples
+        }
+
+        //NW SHINGLE
+        let nw = sh0.nw
+        let ne = new Point(mNorth.x, mNorth.y);
+        let se = new Point(midX, midY);
+        let sw = new Point(mWest.x, mWest.y);
+        let sh = new Shingle(nw, ne, se, sw, pal);
+        shingleList.push(sh);
+
+        if (maintainColor) {
+            pal = sh0.palette
+        } else {
+            pal = cappuccino;
+        }
+
+
+        //NE shingle
+        nw = new Point(mNorth.x, mNorth.y);
+        ne = new Point(sh0.ne.x, sh0.ne.y);
+        se = new Point(mEast.x, mEast.y);
+        sw = new Point(midX, midY);
+        sh = new Shingle(nw, ne, se, sw, pal);
+        shingleList.push(sh);
+
+        if (maintainColor) {
+            pal = sh0.palette
+        } else {
+            pal = red_orange
+        }
+
+        //SE shingle
+        nw = new Point(midX, midY);
+        ne = new Point(mEast.x, mEast.y);
+        se = new Point(sh0.se.x, sh0.se.y);
+        sw = new Point(mSouth.x, mSouth.y);
+        sh = new Shingle(nw, ne, se, sw, pal);
+        shingleList.push(sh);
+
+        if (maintainColor) {
+            pal = sh0.palette
+        } else {
+            pal = green_yellow
+        }
+
+        //SW shingle
+        nw = new Point(mWest.x, mWest.y);
+        ne = new Point(midX, midY);
+        se = new Point(mSouth.x, mSouth.y);
+        sw = new Point(sh0.sw.x, sh0.sw.y);
+        sh = new Shingle(nw, ne, se, sw, pal);
+        shingleList.push(sh);
+        sh0.open = 0; // splitting is done
+        sh0.render = 0;
+
+    } else {
+        sh0.open = 0; //too small, but still should be rendered
+    }
+
+    print('ending subd', shingleList.length)
+}
+
+
+
+
