5138b003a7d7e3ad1bcbd714b90168736d56ad4e
Ram-N <ramnarasimhan@gmail.com>
2021-11-17 23:24:51 -0500

    2021-11-17/daily_sketch shapes bg

---
+++ daily_sketches/2021/2021-11-17/README.md
@@ -0,0 +1,32 @@
+# DAILY SKETCH for 2021-11-17
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-11-17-17-09-31.png' width = '100'> <img src = 'images/keep_2021-11-17-23-23-48.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-11-17-17-09-31.png' width = '300'> 
+<img src = 'images/keep_2021-11-17-23-23-48.png' width = '300'> 
+
+
+
+
+## 2021-11-17
+Keywords: Background
+ 
+
+## Description 
+
+ 4x4 Backgrounds inside panel boxes. Each box/panel has a slightly different background
+ The idea is to use one of the corners and to draw colored shapes. 
+ Just working on creating a set of usable Backgrounds, which I hope to use in future images 
+
+Made using P5.js. | [Code](2021/2021-11-17/) | [Top](#daily-sketches) 
+
+-----
+
---
+++ daily_sketches/2021/2021-11-17/sketch.js
@@ -0,0 +1,78 @@
+// Daily Sketch for 2021-11-17
+// Ram Narasimhan.
+
+/*
+Desc: 3x3 Backgrounds inside panel boxes. (SineBg, brickBg etc.)
+*/
+
+
+let palette = []
+let grid;
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+const params = {
+    xStep: 60,
+    yStep: 60,
+    bgColor: [210, 50, 80],
+    blkColor: [0, 0, 0],
+}
+
+
+const BgParams = {
+    size: "medium",
+    rows: 10,
+    columns: 10,
+    color: "red",
+    sw: 3
+
+}
+
+function setup() {
+    createCanvas(960, 960);
+    colorMode(HSB);
+    background(params.blkColor);
+    print(params.bgColor)
+    palette2 = HrainbowDash; //colors.js
+    palette = Hcappuccino; //colors.js
+    palette = replicate(palette, 100)
+
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+    fill(params.blkColor);
+    rect(cnv.xMargin, cnv.yMargin, cnv.width, cnv.height);
+    pgrid = createSquareGrid(3);
+
+    for (p of pgrid.panels) {
+        push();
+        _box = new Bbox(p.x, p.y, p.w, p.h);
+        //        sineWall(_box, BgParams);
+        diagWall(_box, BgParams);
+        pop();
+    }
+
+    draw_border(clr = params.blkColor, sw = 30 + cnv.xMargin); //rn_utils.js
+}
+
+
+class Bbox { // A bounding box
+    constructor(x, y, w, h) {
+        this.x = x; // nw corner x
+        this.y = y; //nw y
+        this.h = h;
+        this.w = w;
+    }
+
+    display(colr, sw = 1) {
+        strokeWeight(sw)
+        if (colr) {
+            stroke(colr);
+            fill(colr);
+        }
+        rect(this.x, this.y, this.w, this.h)
+    }
+
+
+}
