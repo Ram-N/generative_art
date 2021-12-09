9a0e4a11377ee5b6bd57f219c03f489e21d8252b
Ram-N <ramnarasimhan@gmail.com>
2021-12-08 20:15:04 -0500

    2021-12-03/daily_sketch gandhi

---
+++ daily_sketches/2021/2021-12-03/README.md
@@ -0,0 +1,32 @@
+# DAILY SKETCH for 2021-12-03
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-12-07-14-59-50.png' width = '100'> <img src = 'images/keep_2021-12-07-21-57-17.png' width = '100'> <img src = 'images/keep_2021-12-08-20-14-07.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-12-07-14-59-50.png' width = '300'> 
+<img src = 'images/keep_2021-12-07-21-57-17.png' width = '300'> 
+<img src = 'images/keep_2021-12-08-20-14-07.png' width = '300'> 
+
+
+
+
+## 2021-12-03
+Keywords: images
+ 
+
+## Description 
+
+ Gandhi Experiments. Take patches of Gandhi image and render it for a different effect.
+ 
+
+Made using P5.js. | [Code](2021/2021-12-03/) | [Top](#daily-sketches) 
+
+-----
+
---
+++ daily_sketches/2021/2021-12-03/sketch.js
@@ -0,0 +1,89 @@
+// Daily Sketch for 2021-12-03
+// Ram Narasimhan.
+
+/*
+Keywords: images
+Desc: Composite image made up of vertical strips of 3 images
+*/
+
+let palette = []
+let grid;
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+const params = {
+    xStep: 60,
+    yStep: 60,
+    bgColor: [210, 50, 80],
+    blkColor: [0, 0, 0],
+}
+
+let numImages = 1;
+let imgs = [];
+_str = ['assets/gandhi2.jpg']
+function preload() {
+    for (i = 0; i < numImages; i++) {
+        imgs[i] = loadImage(_str[i]);
+    }
+}
+
+function setup() {
+
+    createCanvas(960, 960);
+    //colorMode(HSB);
+    //background(params.blkColor);
+    print(params.bgColor)
+    palette2 = random(HSBpalList)
+    palette = replicate(palette, 100)
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+    fill(params.blkColor);
+
+    for (i = 0; i < numImages; i++) {
+        print(imgs[i].width)
+        imgs[i].resize(0, 960);
+        imgs[i].loadPixels();
+    }
+
+
+    stepSize = 20;
+    numStrips = int(width / stepSize) + 1;
+    noStroke();
+    for (j = 0; j < numStrips; j++) {
+        for (i = 0; i < numStrips; i++) {
+            x = i * stepSize;
+            y = j * stepSize;
+            patch = imgs[0].get(x, y, stepSize, stepSize);
+            rgb = getAvgPatchColor(patch);
+
+            push();
+            translate(x, y);
+            luma = (rgb.r + rgb.r + rgb.r + rgb.b + rgb.g * 4) >> 3
+            print(luma)
+            renderPatch(rgb, luma);
+            pop();
+
+        }
+    }
+
+
+
+    //    draw_border(clr = params.blkColor, sw = 30 + cnv.xMargin); //rn_utils.js
+}
+
+
+function renderPatch(rgb, luma) {
+    numLines = 5;
+    fill(rgb.r, rgb.g, rgb.b)
+    stroke(rgb.r, rgb.g, rgb.b)
+    if (luma < 128) {
+        rect(0, 0, stepSize, stepSize)
+    } else { //lighter
+        for (l = 0; l < numLines; l++) {
+            xInc = l * (stepSize / numLines)
+            line(xInc, 0, xInc, stepSize)
+        }
+    }
+}
