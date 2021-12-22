3456b8d4b064fb9342ca52389cb0449df0539561
Ram-N <ramnarasimhan@gmail.com>
2021-12-22 11:57:06 -0500

    2021-12-16/daily_sketch fidenza1

---
+++ daily_sketches/2021/2021-12-16/README.md
@@ -0,0 +1,34 @@
+# DAILY SKETCH for 2021-12-16
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-12-22-11-21-16.png' width = '100'> <img src = 'images/keep_2021-12-22-11-32-08.png' width = '100'> <img src = 'images/keep_2021-12-22-11-54-24.png' width = '100'> <img src = 'images/keep_2021-12-22-11-55-01.png' width = '100'> <img src = 'images/keep_2021-12-22-11-55-57.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-12-22-11-21-16.png' width = '300'> 
+<img src = 'images/keep_2021-12-22-11-32-08.png' width = '300'> 
+<img src = 'images/keep_2021-12-22-11-54-24.png' width = '300'> 
+<img src = 'images/keep_2021-12-22-11-55-01.png' width = '300'> 
+<img src = 'images/keep_2021-12-22-11-55-57.png' width = '300'> 
+
+
+
+
+## 2021-12-16
+Keywords: flow field
+ 
+
+## Description 
+
+ Draw segments randomly, in a flow-field
+ 
+
+Made using P5.js. 
+
+-----
+
---
+++ daily_sketches/2021/2021-12-16/sketch.js
@@ -0,0 +1,71 @@
+// Daily Sketch for 2021-12-16
+// Ram Narasimhan.
+
+/*
+Keywords: flow field
+
+Desc: Draw segments randomly, in a flow-field. Gearing up towards a Fidenza
+*/
+
+let palette = []
+let grid;
+const cnv = {
+    xMargin: 30,
+    yMargin: 30,
+}
+
+const params = {
+    xStep: 60,
+    yStep: 60,
+    bgColor: [210, 50, 80],
+    blkColor: [0, 0, 0],
+    moves: 1000
+}
+
+let chosen;
+let rot = 0;
+let shapeRings;
+
+
+function setup() {
+
+    createCanvas(960, 960);
+    colorMode(HSB);
+    background(params.blkColor);
+    print(params.bgColor)
+    palette2 = random(HSBpalList)
+    palette = random(HSBpalList)
+    palette = replicate(palette, 100)
+    cnv.height = height - 2 * cnv.yMargin // usable height
+    cnv.width = width - 2 * cnv.yMargin //usable width
+    fill(params.blkColor);
+
+    //grid = new Grid(params.xStep, params.yStep, cnv.width, cnv.height, cnv.xMargin, cnv.yMargin, createSegs = true);
+
+    tg = new TileGrid(params.xStep, params.yStep, cnv.width, cnv.height, cnv.xMargin, cnv.yMargin)
+
+    fill(random(palette2))
+    for (col = 0; col < tg.cols; col++) {
+        for (row = 0; row < tg.rows; row++) {
+            tile = tg.getTileFromCR(col, row, verbose = false);
+            angle = (col / (2 * tg.cols)) * PI
+            tile.angle = angle //store the flow field
+        }
+    }
+
+
+    for (i = 0; i < 1000; i++) {
+        step = tg.height * random([1, 1.5, 2, 1.8])
+        stroke(random(palette2))
+        strokeWeight(random([3, 4, 5]))
+        x = cnv.xMargin + random(cnv.width)
+        y = cnv.yMargin + random(cnv.height)
+        tile = tg.getTile(x, y)
+        line(x, y, x + step * cos(tile.angle), y + step * sin(tile.angle))
+    }
+
+
+
+    draw_border(clr = params.blkColor, sw = 30 + cnv.xMargin); //rn_utils.js
+}
+
