6cc5b0f75b338015c53cdd8efb8045ba0af87f0f
Ram-N <ramnarasimhan@gmail.com>
2021-12-26 13:29:19 -0500

    2021-12-23/daily_sketch polygons

---
+++ daily_sketches/2021/2021-12-23/README.md
@@ -0,0 +1,29 @@
+# DAILY SKETCH for 2021-12-23
+
+## Done using P5.js
+
+### Description
+
+These `daily sketches` which are meant to be quick explorations     on whatever topic interested me on that day. This code is not typically optimized, but I share it as-is     for anyone interested.
+
+<img src = 'images/keep_2021-12-26-13-28-06.png' width = '100'> 
+
+## Progression of Images that were generated.
+
+<img src = 'images/keep_2021-12-26-13-28-06.png' width = '300'> 
+
+
+
+
+## 2021-12-23
+Keywords: Polygons, Hexagonal grid 
+
+## Description 
+
+ Placing Polygons and circles on an isometric grid. The transparent colors lead to interesting effects.
+ 
+
+Made using P5.js. 
+
+-----
+
---
+++ daily_sketches/2021/2021-12-23/sketch.js
@@ -0,0 +1,79 @@
+// Daily Sketch for 2021-12-23
+// Ram Narasimhan.
+/*
+HexGrid.
+2. Draw all the traingles, using 3 colors...
+3. Create a triangle class, ID = its grid position
+4. each triangle has 3 neighbors = s, t and d(own)
+5. Code in some color rules for neighbors
+
+*/
+let palette = []
+const cnv = {
+  xMargin: 30,
+  yMargin: 30,
+}
+
+const params = {
+  tw: 80, // triangle width of the hexgrid...
+  xStep: 60,
+  yStep: 60,
+  bgColor: [210, 50, 80],
+  blkColor: [0, 0, 0],
+}
+
+
+function setup() {
+  createCanvas(860, 860);
+  background("#d3d3d3");
+  palette = red_brown_orange; //colors.js
+  //palette = purples; //colors.js
+  palette = replicate(palette, 10)
+
+
+  cnv.height = height - 2 * cnv.yMargin // usable height
+  cnv.width = width - 2 * cnv.yMargin //usable width
+  fill(params.blkColor);
+  rect(cnv.xMargin, cnv.yMargin, cnv.width, cnv.height)
+
+
+  hg = new HexGrid(cnv.width, cnv.height, cnv.xMargin, cnv.yMargin, params.tw);
+  stroke(255)
+  //renderGridPoints(gridPts) // grid.js
+  let alphaValue = 200;
+  print(hg.points.length) // columns. each one is a column of points
+
+  //fill("#1167b1")
+  stroke(0);
+  strokeWeight(5)
+  //base_r = cnv.width / (25)
+  base_r = params.tw
+
+
+  for (co of hg.points) { //using only the center points...
+    for (gpt of co) {
+      push();
+
+      translate(gpt.x, gpt.y) // go to grid point center
+      rad = base_r * random([0.5, 0.25, 0.75])
+
+      if (random(1) < 0.35) {
+        c = color(random(palette))
+        c.setAlpha(alphaValue);
+        fill(c);
+        circle(0, 0, 2 * rad)
+      } else {
+        hx = new Polygon(0, 0, random([6, 8]), rad, random(purples)); //rn_shapes.js
+        strokeWeight(int(random(3, 10)));
+        rotate(PI / 7 * int(random(7)))
+        hx.render()
+        //hx.renderVertices();
+      }
+      pop();
+    }
+  }
+
+  draw_border(clr = params.blkColor, sw = 30 + cnv.xMargin); //rn_utils.js
+}//end setup
+
+
